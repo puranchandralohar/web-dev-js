7b97e7d4925f409436a4a29bacbf6c508c04e9f4
puranchandralohar <clpuran95@gmail.com>
2024-03-18 13:39:26 +0530

    day-1

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("hello"); //text or String
+console.log(5); //number
+
+alert("Hello World");
+
+// Variable
+
+let firstname;
+let lastname; // ; is used to terminate the line
+
+firstname = "Jhon"; // "", '' used for Text / String
+lastname = "Cena";
+
+lastname = "Mark";
+console.log("Name = " ,firstname,lastname);
+
+const firstNumber = 8; //decleared + Assigned
+
+firstNumber = 10; // Can not Reassign
+
+console.log("Number - ", firstNumber);
+
+
+
+
+
+
+
+
+
+
