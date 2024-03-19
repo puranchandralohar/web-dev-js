66ba1e6476b6830620155a97a79eda6f9e735c99
puranchandralohar <clpuran95@gmail.com>
2024-03-19 13:36:52 +0530

    day-2

---
+++ app1.js
@@ -0,0 +1,88 @@
+// let button = document.getElementById('btn'); // button
+// let text = document.getElementById('text'); //text
+
+// button.addEventListener('click', function(){  //change text on button click
+//     text.innerHTML = "Hii";
+// });
+
+// Variable - A variable is a container for storing data values.
+
+// let and const are used to declare a variable. (ES6) - 2015
+
+// var is also used to declare a variable but it is not recommended.
+
+// let firstname; //variable declearation
+// let lastname; //variable declearation
+
+// firstname = "Jhon"; //variable assignment  
+// firstname = "Mark"; //variable reassignment
+ 
+//  = is used to assign value to a variable , assignment operator
+// = "" , '' used for Text / String
+
+// let fullname = "Punam" //variable declearation + assignment
+
+// fullname = "Punam Singh" //variable reassignment
+
+// let fullname = 9 //variable redeclaration
+
+// We can not redeclare a variable using let keyword.
+
+// identifier - name of the variable
+
+// console.log(fullname);
+
+// const firstNumber = 9; //variable declearation + assignment
+// firstNumber = 10; //variable reassignment
+
+// const firstNumber = 10; //variable redeclaration
+
+// in const , we can not reassign a variable and we can not redeclare a variable.
+
+console.log("first", firstNumber);
+
+let firstNumber = 10;
+
+// Hoisting- JavaScript hoisting is the process of moving all the declarations to the top of the current scope (block scope / functional scope).
+
+// const firstNumber = 10; //variable redeclaration
+
+// console.log(firstname); //output - Jhon
+// console.log(lastname); //output - undefined
+
+//  var - global scope / functional scope
+// let and const - block scope
+
+// var - hositing
+// let and const - no hositing
+
+// Rule for naming JS variable:
+
+// 1. Variable names can only contain letters, numbers, $ and _.\
+// 2. Variable names can not start with a number.
+// 3. Variable names are case sensitive.
+// 4. Variable names can not contain spaces.
+// 5. Variable names should be descriptive.
+// 6. Variable names can not contain reserved words.
+
+
+// MultipleWords - camelCase,
+
+// namingConvention
+// camelCase; - JS
+// PascalCase; - React
+// snake_case; - Python
+// kebab-case, dash-case; - CSS
+
+
+
+// firstname = "Punam"; //valid
+
+// firstName;
+// fullname;
+// goingToShopping;
+
+
+// let name@name = "Punam"; //invalid
+
+// let 9name = "Punam"; //invalid
