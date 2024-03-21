3ce53e24206a783252e977dd60dc0a32b4d84d09
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-03-21 13:16:20 +0530

    code added

---
+++ app2.js
@@ -0,0 +1,106 @@
+// Assignment Operators --> =, +=, -=, *=, /=, %=, **=
+
+// let x = 5;
+// x = x + 10;
+// console.log(x);
+
+// x += 10; // same as x = x + 10;
+// console.log(x);
+
+// let a = "JavaScipt";
+// a += " is Awesome";
+// console.log(a);
+
+// x -= 2; // same as x = x - 2;
+// console.log(x);
+
+// x *= 2; // same as x = x * 2;
+// console.log(x);
+
+// x /= 2; // same as x = x / 2;
+// console.log(x);
+
+// x %= 2; // same as x = x % 2;
+// console.log(x);
+
+// x **= 2; // same as x = x ** 2;
+// console.log(x);
+
+
+
+// Comparison Operators --> ==, ===, !=, !==, >, <, >=, <=
+
+// let a = 5;
+// let b = "5";
+
+// console.log(a == b); // checks only value
+// console.log(a === b); // checks value and datatype
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// let a = "Java";
+// let b = "Script";
+// let output  = a < b;
+// console.log(output);
+
+// let x = "A";
+// let y = "B";
+// let output = x < y;
+// console.log(output);
+
+// let a = "Amit";
+// let b = "Kumar";
+// let z = a + " " + b;
+// console.log(z);
+
+// let x = 5;
+// let y = "10" + x;
+// console.log(y);
+
+
+
+// Logical Operators --> &&, ||, !
+
+// let a = 5;
+// let b = 10;
+// console.log(a == b && a < b);
+// console.log(a == b || a < b);
+// console.log(!(a == b));
+
+// let a = 5;
+// let b = 2;
+// console.log(a**b);
+// console.log(Math.pow(a,b));
+
+// Primitive Data Types --> Number, String, Boolean, Undefined, Null, Symbol, BigInt
+// Reference / Non-Primitive Data Types --> Object, Array, Function
+
+
+// Template literals - backticks(``)
+
+// let fName = "Punam";
+// let lName = "Kumari";
+// console.log("My Name is " + fName + " " + lName);
+// console.log(`My Name is ${fName} ${lName}`);
+
+// let a = 5;
+// let b = 10;
+// let total = "Total amount is " + (a + b) + " only";
+// let total = `Total amount is ${a + b} only`;
+// console.log(total);
+
+// let fName = "Punam";
+// console.log(`Hello ${fName}`);
+
+// let a = 10;
+// console.log(++a);
+// console.log(a++);
+// console.log(a);
+// console.log(--a);
+// console.log(a);
+// console.log(a--);
+// console.log(a);
