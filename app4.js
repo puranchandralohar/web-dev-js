a1be07bc3dff16ec928641c8d983dafd33181226
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-03-26 13:17:17 +0530

    new topics

---
+++ app4.js
@@ -0,0 +1,94 @@
+// Falsy values - false, 0, "", null, undefined, NaN
+
+// let isOpen = false;
+
+// if (isOpen) {
+//     console.log("The door is open");
+// } else {
+//     console.log("The door is closed");
+// }
+
+// let apples = 0;
+
+// if (apples) {
+//     console.log("We have apples");
+// } else {
+//     console.log("We have no apples");
+// }
+
+// let input = "";
+// // let message = input || "Hello!";
+// let message = "Hello!" || input;
+// console.log(message);
+
+// let fname = null;
+// if (fname) {
+//     console.log("We have a name");
+// } else {
+//     console.log("We have no name");
+// }
+
+// let fname;
+// if(fname) {
+//     console.log("Hi!" + fname);
+// } else {
+//     console.log("Hi! Guest");
+// }
+
+
+
+// let num1 = "Two";
+// let num2 = 2;
+
+// let result = num1/num2;
+// console.log(result);
+
+// if(isNaN(result)) {
+//     console.log("The result is not a number");
+// } else {
+//     console.log(result);
+// }
+
+
+
+// console.log(Boolean("false"));
+// console.log(Boolean(" "));
+// console.log(Boolean("0"));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+
+
+// String and Number comparison
+
+// console.log("Z" > "A");
+// console.log("A" > "B");
+// console.log("Priyanka" > "Punam");
+// console.log("Mee" > "Me");
+// console.log("Priya" > "Priyanka");
+// console.log("a" > "A");
+
+// console.log("2" > 1); // "2" will convert to 2
+// console.log("01" == 1); // "01" will convert to 1
+// console.log("A" > 0);
+// console.log("A" == 65);
+
+// console.log(true > 0);
+// console.log(false > 0);
+// console.log(true == 1);
+// console.log(false == 0);
+
+// console.log("0" == 0);
+// console.log(Boolean("0"));
+// console.log(Boolean(0));
+
+// console.log(typeof("0"));
+// console.log(typeof(0));
+// console.log(typeof(false));
+
+// console.log(false === 0);
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
