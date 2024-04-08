ed242b67dd245eb2de5ea524425e5079755c7081
puranchandralohar <clpuran95@gmail.com>
2024-04-08 13:57:43 +0530

    functions basic

---
+++ function.js
@@ -0,0 +1,71 @@
+// Write Ones , Use Many Times
+
+// function functionName() {
+//   action, statment, code
+// }
+
+// fuunction defination, function statment
+
+// function generateMessage() {
+//   console.log("Hello World");
+// }
+
+// generateMessage(); // calling the function, invoke the function, execute the function
+
+// generateMessage();
+// generateMessage();
+// generateMessage();
+
+// function with parameters
+// Number, string, boolean, object, array, function
+
+// let number = 10;
+
+// function printNumber(number){
+
+//     return (number.luckyNo * number.n) + 5
+
+// }
+
+// function passingNumber(num){  // num - parameter
+//     return num;
+// }
+
+// const result = passingNumber(10)*printNumber({"name":"Sangeeta","luckyNo":7,"n":5}); //number - argument
+
+// console.log(result);
+
+// let array = [10, 20, 30, 40, 50];
+
+// function passingArrays(arr){
+//     arr[0] = 100;
+//     console.log(arr);
+// }
+// console.log(array[0]); // 10
+// passingArrays(array); //100
+
+// printNum([10,20,30,40]);
+
+// const obj = {
+//     name: "Jhon",
+//     age: 22
+// }
+
+// function printObject(obj){
+//     console.log(obj);
+// }
+
+// function decleration
+// function printObject() {
+//   return 10;
+// }
+
+// printObject();
+
+// // function expression
+// const s = function () {
+//   // anonymous function
+//   console.log("Hello World");
+// };
+
+// s();
