efde5f27210eb3c20a701b24d9538e1e5027c90f
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-04-16 13:09:23 +0530

    object basics

---
+++ Object.js
@@ -0,0 +1,63 @@
+// Learn about primitive and non-primitive data types
+
+// Objects
+
+// Object literal
+
+// let user = {
+//     name: 'Alka',
+//     age: 20,
+// };
+
+// console.log(user);
+// console.log(user.name);
+// console.log(user.age);
+
+// user.city = 'Delhi';
+
+// console.log(user);
+
+// delete user.age;
+
+// console.log(user);
+
+// let keys = Object.keys(user);
+// console.log(keys);
+
+// let key1 = Object.keys(user)[0];
+// let key2 = Object.keys(user)[1];
+// console.log(key1, key2);
+
+// let user1 = {
+//     name: 'Alka',
+//     age: 20,
+//     "email id": "alka@navgurukul.org", // key with space
+// };
+
+// console.log(user1);
+// console.log(user1.name);
+// console.log(user1.age);
+// console.log(user1["email id"]);
+// console.log(user1["name"]);
+
+// delete user1["email id"];
+
+// console.log(user1);
+
+// let user = {
+//     name: 'Alka',
+//     age: 20,
+// };
+// console.log(user);
+// console.log(user.name);
+// console.log(user.city);
+
+// let fName = "Vaishnavi";
+
+// let user2 = {
+//     [fName]: "Anjali",
+// };
+
+// console.log(user2);
+// console.log(user2.fName);
+// console.log(user2.Vaishnavi);
