e74b4922c17f33c71be8de20d271f81ed4dad1bd
puranchandralohar <clpuran95@gmail.com>
2024-04-23 11:27:27 +0530

    array-questions

---
+++ array-questions.js
@@ -0,0 +1,15 @@
+
+// Sum of Array Elements: Write a function to calculate the sum of all elements in an array of integers.
+
+// Average of Array Elements: Write a function to calculate the average of all elements in an array of integers.
+
+// Find Maximum Element: Write a function to find the maximum element in an array of integers.
+
+// Find Minimum Element: Write a function to find the minimum element in an array of integers.
+
+// Array Search: Write a function to search for a given element in an array and return its index if found, otherwise return -1.
+
+// Array Sorting: Write a function to sort an array of integers in non-decreasing order (ascending).
+
+// Array Filtering: Write a function to filter out even numbers from an array of integers and return a new array containing only the even numbers.
+
