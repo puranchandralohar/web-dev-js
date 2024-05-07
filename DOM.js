8341c97822b8051006d2a3b65d5084f2f283e02d
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-05-07 12:59:04 +0530

    DOM continues

---
+++ DOM.js
@@ -0,0 +1,16 @@
+console.log('DOM.js loaded');
+
+// let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
+
+// boxes[0].style.backgroundColor = "red";
+
+// boxes[2].style.backgroundColor = "red";
+
+// document.getElementById("red").style.backgroundColor = "red";
+
+// document.querySelector(".box").style.backgroundColor = "red";
+
+// document.querySelector("#red").style.backgroundColor = "red";
+
+console.log(document.querySelectorAll(".box"));
