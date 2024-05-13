d9a4e215eae0e75b69d66166609d8d9e63662454
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2024-05-13 13:02:05 +0530

    event bubbling& capturing

---
+++ EventBubbling.js
@@ -0,0 +1,152 @@
+// Event Bubbling
+// Event Capturing or trickling
+
+// Event Bubbling
+
+const grandParent = document.querySelector('.grandParent');
+const parent = document.querySelector('.parent');
+const child = document.querySelector('.child');
+
+// If we want to stop the event bubbling we can use e.stopPropagation() method
+
+
+// grandParent.addEventListener("click", (e) => {
+//     e.stopPropagation();
+//     // console.log('grandParent bubbling');
+//     alert('grandParent bubbling');
+// })
+
+// parent.addEventListener("click", (e) => {
+//     e.stopPropagation();
+// //   console.log("parent bubbling");
+//     alert('parent bubbling');
+// });
+
+// child.addEventListener("click", (e) => {
+//     e.stopPropagation();
+// //   console.log("child bubbling");
+//     alert('child bubbling');
+// });
+
+
+// Event Capturing or trickling
+
+// grandParent.addEventListener("click", (e) => {
+//     e.stopPropagation();
+//     console.log('grandParent capturing');
+// },
+// {capture: true}
+// )
+
+// parent.addEventListener(
+//   "click",
+//   (e) => {
+//     e.stopPropagation();
+//     console.log("parent capturing");
+//   },
+//   { capture: true }
+// );
+
+// child.addEventListener(
+//   "click",
+//   (e) => {
+//     e.stopPropagation();
+//     console.log("child capturing");
+//   },
+//   { capture: true }
+// );
+
+
+// If we want to capture the element only once then we can use {once: true} in the event listener
+//For event bubbling
+
+// grandParent.addEventListener(
+//   "click",
+//   (e) => {
+//     // e.stopPropagation();
+//     console.log("grandParent bubbling");
+//   },
+//   { once: true}
+// );
+
+// parent.addEventListener(
+//   "click",
+//   (e) => {
+//     // e.stopPropagation();
+//     console.log("parent bubbling");
+//   },
+//   { once: true}
+// );
+
+// child.addEventListener(
+//   "click",
+//   (e) => {
+//     e.stopPropagation();
+//     console.log("child bubbling");
+//   },
+//   { once: true}
+// );
+
+
+//For event capturing
+
+// grandParent.addEventListener(
+//   "click",
+//   (e) => {
+//     e.stopPropagation();
+//     console.log("grandParent capturing");
+//   },
+//   { once: true, capture: true}
+// );
+
+// parent.addEventListener(
+//   "click",
+//   (e) => {
+//     // e.stopPropagation();
+//     console.log("parent capturing");
+//   },
+//   { once: true, capture: true}
+// );
+
+// child.addEventListener(
+//   "click",
+//   (e) => {
+//     // e.stopPropagation();
+//     console.log("child capturing");
+//   },
+//   { once: true, capture: true}
+// );
+
+
+
+
+// To remove the element after a given time
+
+// function printGrandParent(){
+//     console.log('grandParent bubbling');
+// }
+
+// grandParent.addEventListener("click", printGrandParent);
+
+// setTimeout(() => {
+//     grandParent.removeEventListener("click", printGrandParent);
+// }, 2000)
+
+
+// To remove the element after a given time using a button
+
+const button = document.querySelector('#btn');
+
+function printGrandParent(){
+    console.log('grandParent bubbling');
+}
+
+grandParent.addEventListener("click", printGrandParent);
+
+function removeGrandParent(){
+    setTimeout(() => {
+        grandParent.removeEventListener("click", printGrandParent);
+    }, 2000)
+}
+
+button.addEventListener('click', removeGrandParent);
